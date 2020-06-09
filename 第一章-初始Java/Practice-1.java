107fbe9af37655fc3386223ec25351a38025e99f
zhaijj <zhaijingjing603@gmail.com>
2020-06-09 22:40:38 +0800

    update

---
+++ 第一章-初始Java/Practice-1.java
@@ -0,0 +1,6 @@
+// 实践与练习p17
+public class FirstJava{
+	public static void main(String[] args) {
+		System.out.println("这是我的第一个Java程序");
+	}
+}
---
+++ 第一章-初始Java/Practice-2.java
@@ -0,0 +1,9 @@
+//实践与练习p17
+public class formatPrint{
+	public static void main(String[] args) {
+		System.out.println("---------------");
+		System.out.println(":  我要学会  :");
+		System.out.println(":  Java语言  :");
+		System.out.println("---------------");
+	}
+}
